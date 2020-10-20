bf0f8bcead2295178fa72055dbd244d95f52c6e2
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-20 19:20:00 +0200

    Add a CORS handler

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"main.go/handlers"
 )
@@ -41,10 +42,13 @@ func main() {
 	getRouter.Handle("/docs", sh)
 	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	// CORS
+	corsHandler := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
+
 	// create the server
 	server := &http.Server{
 		Addr:         ":8080",
-		Handler:      serverMux,
+		Handler:      corsHandler(serverMux),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
