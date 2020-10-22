abb48b73c545803a94968afadea0ad984400661b
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-22 23:32:09 +0200

    Move documentation specs to seperate file

---
+++ handlers/docs.go
@@ -0,0 +1,16 @@
+// Package classification of Quiz API
+//
+// Documentation for Quiz API
+//
+//  Schames: http
+//	BasePath: /
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//	swagger:meta
+
+package handlers
--- handlers/user.go
+++ handlers/user.go
@@ -1,17 +1,3 @@
-// Package classification of Quiz API
-//
-// Documentation for Quiz API
-//
-//  Schames: http
-//	BasePath: /
-//	Version: 1.0.0
-//
-//	Consumes:
-//	- application/json
-//
-//	Produces:
-//	- application/json
-//	swagger:meta
 package handlers
 
 import (
