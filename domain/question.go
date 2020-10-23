28e9c7f70fda35e1bdd6bf8a10a36a0d2b920b0b
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-23 19:52:17 +0200

    Anotate domain models

--- domain/question.go
+++ domain/question.go
@@ -2,10 +2,10 @@ package domain
 
 // Question domain model
 type Question struct {
-	ID        int    `json:"id"`
-	Text      string `json:"text"`
-	Answers   []Answer
-	CreatedOn string `json:"-"`
-	UpdatedOn string `json:"-"`
-	DeletedOn string `json:"-"`
+	ID        int      `json:"id"`
+	Text      string   `json:"text"`
+	Answers   []Answer `json:"answers"`
+	CreatedOn string   `json:"-"`
+	UpdatedOn string   `json:"-"`
+	DeletedOn string   `json:"-"`
 }
--- domain/quiz.go
+++ domain/quiz.go
@@ -2,11 +2,11 @@ package domain
 
 // Quiz domain model
 type Quiz struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	Timer     string `json:"timer"`
-	Questions []Question
-	CreatedOn string `json:"-"`
-	UpdatedOn string `json:"-"`
-	DeletedOn string `json:"-"`
+	ID        int        `json:"id"`
+	Name      string     `json:"name"`
+	Timer     string     `json:"timer"`
+	Questions []Question `json:"questions"`
+	CreatedOn string     `json:"-"`
+	UpdatedOn string     `json:"-"`
+	DeletedOn string     `json:"-"`
 }
