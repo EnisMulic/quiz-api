52346bed2e170c491bf8a01eb2bd1cfc4cc1872c
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-28 22:22:43 +0100

    Add auth middleware

---
+++ handlers/middleware.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var secretKey = []byte("gosecretkey")
+
+// IsAuthorized checks if user is authorized
+func IsAuthorized(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return secretKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(rw, err.Error())
+			}
+
+			if token.Valid {
+				next.ServeHTTP(rw, r)
+			}
+		} else {
+
+			fmt.Fprintf(rw, "Not Authorized")
+		}
+	})
+}
